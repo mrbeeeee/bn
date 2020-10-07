edb3b22fe41a3d2312ed637cc8f17d2be3997519
mrbeeeee <57273057+mrbeeeee@users.noreply.github.com>
2020-10-07 18:06:49 -0400

    yes

--- a2020bell.js
+++ a2020bell.js
@@ -171,9 +171,9 @@ function scheduleA() {
         // Get today's date and time
         var now = new Date().getTime();
 
-        // Find the distance between now and the count down date
+        // Find the distance between now and the count down datex
         distance = countDownDate - now;
-        document.getElementById("demo4-a").innerHTML = distance;
+        document.getElementById("demo2-a").innerHTML = distance;
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
